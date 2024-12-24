cb9541a5d2e6a2003e8aed6526efca66bb0b680b
Z1vran <aliman0198a@gmail.com>
2024-12-24 10:32:27 +0300

    first

---
+++ DllLibrary/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllLibrary
+{
+    // класс - здание
+    public class Building
+    {
+        // квартиры данного здания
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public List<Apartment> Apartments = new List<Apartment>();
+        public Building(string nm, string ad)
+        {
+            Address = nm;
+            Description = ad;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Здание: адрес = {Address}, описание = {Description}");
+        }
+    }
+    //  класс квартир
+    public class Apartment
+    {
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public Apartment(string nm, string avt)
+        {
+            Number = nm;
+            Owner = avt;
+        }
+        public void Show()
+        {
+            Console.WriteLine(
+                 $"Квартира: номер = {Number}, владелец = {Owner}");
+        }
+    }
+}
---
+++ MainProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+
+namespace MainProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building ps = new Building("Троицкая 11", "дом");
+            ps.Show();
+
+            Apartment b1 = new Apartment("24", "Омуралиев");
+            ps.Apartments.Add(b1);
+
+            Apartment b2 = new Apartment("16", "Арутюнян");
+            ps.Apartments.Add(b2);
+
+            foreach (Apartment b in ps.Apartments)
+            {
+                b.Show();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
